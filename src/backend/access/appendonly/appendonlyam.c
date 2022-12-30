fdc94a7adf8bb8a5620b201827df7549a3da9902
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-12-30 13:06:43 -0800

    ao: Scan progress reporting for CREATE INDEX
    This enables reporting for the table scan phase of index builds on AO
    tables. This directly affects:
    (1) pg_stat_progress_create_index.blocks_total
    (2) pg_stat_progress_create_index.blocks_done
    
    Sample:
    
    Connect in utility mode to a segment, while CREATE INDEX is running on
    an AO table:
    
    postgres=# select pid, command, phase, blocks_total, blocks_done from pg_stat_progress_create_index;
      pid   |   command    |             phase              | blocks_total | blocks_done
    --------+--------------+--------------------------------+--------------+-------------
     633034 | CREATE INDEX | building index: scanning table |        54993 |        4412
    (1 row)
    
    This commit introduces:
    
    (1) Accounting in AppendOnlyScanDescData to track the total number of
    bytes read across all segment files. It is updated whenever a new block
    is read. If the block is compressed, its compressed length is added
    (uncompressed length otherwise).
    
    (2) Reporting code in appendonly_index_build_range_scan() to report in
    increments of BLCKSZ, the number of blocks scanned so far. The total
    number of blocks to be scanned is also published up front and is
    determined by summing the compressed eof values of all segfiles.
    
    (3) Marks RelationGuessNumberOfBlocksFromSize() as inline, since it is
    now being called in a much hotter code path.
    
    Note: We currently only support reporting for non-concurrent index
    builds and serial builds.

--- src/backend/access/appendonly/appendonlyam.c
+++ src/backend/access/appendonly/appendonlyam.c
@@ -104,6 +104,8 @@ static void appendonly_insert_finish_guts(AppendOnlyInsertDesc aoInsertDesc);
 
 /* Hook for plugins to get control in appendonly_delete() */
 appendonly_delete_hook_type appendonly_delete_hook = NULL;
+static void AppendOnlyScanDesc_UpdateTotalBytesRead(
+										AppendOnlyScanDesc scan);
 
 /* ----------------
  *		initscan - scan code common to appendonly_beginscan and appendonly_rescan
@@ -1250,6 +1252,8 @@ getNextBlock(AppendOnlyScanDesc scan)
 	AppendOnlyExecutorReadBlock_GetContents(
 											&scan->executorReadBlock);
 
+	AppendOnlyScanDesc_UpdateTotalBytesRead(scan);
+
 	return true;
 }
 
@@ -1580,6 +1584,9 @@ appendonly_beginrangescan_internal(Relation relation,
 							   AccessShareLock,
 							   appendOnlyMetaDataSnapshot);
 	}
+
+	scan->totalBytesRead = 0;
+
 	return scan;
 }
 
