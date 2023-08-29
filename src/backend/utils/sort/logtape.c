690131235efda1bb746ac76eb7435517f25c7501
Michael Paquier <michael@paquier.xyz>
2023-08-30 08:03:52 +0900

    Avoid possible overflow with ltsGetFreeBlock() in logtape.c
    nFreeBlocks, defined as a long, stores the number of free blocks in a
    logical tape.  ltsGetFreeBlock() has been using an int to store the
    value of nFreeBlocks, which could lead to overflows on platforms where
    long and int are not the same size (in short everything except Windows
    where long is 4 bytes).
    
    The problematic intermediate variable is switched to be a long instead
    of an int.
    
    Issue introduced by c02fdc9223015, so backpatch down to 13.
    
    Author: Ranier vilela
    Reviewed-by: Peter Geoghegan, David Rowley
    Discussion: https://postgr.es/m/CAEudQApLDWCBR_xmwNjGBrDo+f+S4E87x3s7-+hoaKqYdtC4JQ@mail.gmail.com
    Backpatch-through: 13

--- src/backend/utils/sort/logtape.c
+++ src/backend/utils/sort/logtape.c
@@ -396,7 +396,7 @@ ltsGetFreeBlock(LogicalTapeSet *lts)
 {
 	long	   *heap = lts->freeBlocks;
 	long		blocknum;
-	int			heapsize;
+	long		heapsize;
 	unsigned long pos;
 
 	/* freelist empty; allocate a new block */
